6108ab00a73717c9901a6f477707850f79fcd430
Lei Xia <lei.xia.personal@gmail.com>
2023-01-11 19:47:06 +0000

    ci script and pdf gen vars

--- src/main/java/com/lei/xia/jasper/service/impl/PdfDocumentPrintService.java
+++ src/main/java/com/lei/xia/jasper/service/impl/PdfDocumentPrintService.java
@@ -5,7 +5,7 @@ import com.lei.xia.jasper.model.Movement;
 import com.lei.xia.jasper.service.DocumentPrintService;
 import java.util.HashMap;
 import java.util.List;
-import lombok.RequiredArgsConstructor;
+import java.util.Map;
 import net.sf.jasperreports.engine.JRException;
 import net.sf.jasperreports.engine.JasperExportManager;
 import net.sf.jasperreports.engine.JasperFillManager;
@@ -14,18 +14,23 @@ import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
 import org.springframework.stereotype.Service;
 
 @Service
-@RequiredArgsConstructor
 public class PdfDocumentPrintService implements DocumentPrintService<Movement, byte[]> {
 
-  private final JasperReport movementJasperReport;
 
+  private final JasperReport movementJasperReport;
+  private Map<String, Object> variables;
+
+  public PdfDocumentPrintService(JasperReport movementJasperReport, Map<String, Object> variables) {
+    this.movementJasperReport = movementJasperReport;
+    if (variables == null) {
+      this.variables = new HashMap<>();
+    }
+  }
   public byte[] generate(Movement source) {
     var dataSource = new JRBeanCollectionDataSource(List.of(source));
     try {
-      var vars = new HashMap<String, Object>(1);
-      vars.put("Author", "Lei Xia");
       var jasperPrinter = JasperFillManager.fillReport(movementJasperReport,
-          vars, dataSource);
+          this.variables, dataSource);
       return JasperExportManager.exportReportToPdf(jasperPrinter);
     } catch (JRException e) {
       throw new PdfGenerationException(String.format("Error: fail to generate report %s",
--- src/test/java/com/lei/xia/jasper/service/impl/PdfDocumentPrinterServiceIntTest.java
+++ src/test/java/com/lei/xia/jasper/service/impl/PdfDocumentPrinterServiceIntTest.java
@@ -3,6 +3,8 @@ package com.lei.xia.jasper.service.impl;
 import com.lei.xia.jasper.config.JasperTemplateFactory;
 import com.lei.xia.jasper.model.Movement;
 import java.io.FileOutputStream;
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.UUID;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
@@ -16,7 +18,10 @@ class PdfDocumentPrintServiceIntTest {
   static void beforeAll() {
     var config = new JasperTemplateFactory();
     var jasperReport = config.movementJasperReport("movement.jrxml");
-    pdfDocumentPrintService = new PdfDocumentPrintService(jasperReport);
+    pdfDocumentPrintService = new PdfDocumentPrintService(jasperReport, new HashMap<>() {{
+      put("author", "The UK Home Office");
+      put("report-type", "movement report");
+    }});
   }
 
   @Test
